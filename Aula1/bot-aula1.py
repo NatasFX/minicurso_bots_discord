aed27a819a1bb7a53eaef53efa55218bec96a362
Natã Schmitt <61515309+NatasFX@users.noreply.github.com>
2022-12-10 04:43:08 -0300

    Create bot-aula1.py

---
+++ Aula1/bot-aula1.py
@@ -0,0 +1,17 @@
+import discord
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print(f'Logado com sucesso como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower().startswith('!oi'):
+        await message.channel.send(f'Oii {message.author.mention}!')
+
+client.run('COLE SEU TOKEN AQUI DENTRO, NÃO REMOVA AS ASPAS SIMPLES')
