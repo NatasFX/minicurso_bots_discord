4b6609bbef70d9a062602ef1c6eae76c5aac61a0
Natã Schmitt <61515309+NatasFX@users.noreply.github.com>
2022-12-10 04:42:30 -0300

    Update bot.py

--- Bot do servidor/bot.py
+++ Bot do servidor/bot.py
@@ -1,4 +1,5 @@
 import discord
+from secret import TOKEN
 
 client = discord.Client(intents=discord.Intents.all())
 
@@ -10,6 +11,7 @@ async def on_ready():
 # esta função é executada em todo novo usuário que entra nos servidores onde o bot está
 @client.event
 async def on_member_join(member):
+    
     if not member.bot:
         await client.get_channel(1051004621264523358).send(f'Bem-vindo usuário {member.name}!')
         cargo_id = 1051022995067519006
@@ -24,7 +26,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower().startswith('!oi'):
-        await message.channel.send(f'Oii {message.author.mention}!')
+    if message.content.lower().startswith('!hehe'):
+        await message.channel.send(f'HMM alguém olhou o github do minicurso e achou um comando escondido! {message.author.mention}')
 
-client.run('TOKEN')
+client.run(TOKEN)
